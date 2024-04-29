f085e1af8efc16f4380b7e6e49be8f2b97308ef9
100-oC <mitotonngoria@outlook.jp>
2024-04-29 17:36:39 +0900

    title

--- src/Scene/Title/Title.cpp
+++ src/Scene/Title/Title.cpp
@@ -5,6 +5,18 @@
 //‰Šú‰»
 void Title::Init()
 {
+	memset(x, 0, sizeof(int));
+	memset(y, 0, sizeof(int));
+
+	for (int i = 0; i < IMAGE_MAX_NUM; i++)
+	{
+		handle[i] = LoadGraph(IMAGE_PATH[i]);
+	}
+
+	x[START] = SCREEN_SIZE_X / 2 - START_SIZE / 2;
+	y[START] = SCREEN_SIZE_Y / 2 + 100;
+
+	mouse = 0;
 
 	//’Êíˆ—‚ÖˆÚ“®
 	g_CurrentSceneID = SCENE_ID_LOOP_TITLE;
@@ -13,8 +25,17 @@ void Title::Init()
 //’Êíˆ—
 void Title::Step()
 {
+	GetMousePoint(&x[MOUSE_1], &y[MOUSE_1]);
 
-	if (Input::Mouse::Push(MOUSE_INPUT_LEFT))
+	mouse = 0;
+	if (Collision::Rect(
+		x[START]+100, y[START]+100, START_SIZE-160, START_SIZE-160,
+		x[MOUSE_1], y[MOUSE_1], MOUSE_SIZE, MOUSE_SIZE))
+	{
+		mouse = 1;
+	}
+
+	if (Input::Mouse::Push(MOUSE_INPUT_LEFT) && mouse == 1)
 	{
 		g_CurrentSceneID = SCENE_ID_FIN_TITLE;
 	}
@@ -23,7 +44,12 @@ void Title::Step()
 //•`‰æˆ—
 void Title::Draw()
 {
+	for (int i = 0; i <= START; i++)
+	{
+		DrawGraph(x[i], y[i], handle[i], true);
+	}
 
+	DrawGraph(x[MOUSE_1], y[MOUSE_1], handle[MOUSE_1 + mouse], true);
 }
 
 //I—¹ˆ—
--- src/Scene/Title/Title.h
+++ src/Scene/Title/Title.h
@@ -2,8 +2,6 @@
 
 class Title
 {
-private:
-
 public:
 	//èâä˙âª
 	void Init();
@@ -13,4 +11,32 @@ public:
 	void Draw();
 	//èIóπèàóù
 	void Fin();
+
+	enum IMAGE_ID
+	{
+		BACKGROUND,
+		START,
+
+		MOUSE_1,
+		MOUSE_2,
+
+		IMAGE_MAX_NUM
+	};
+	const char IMAGE_PATH[IMAGE_MAX_NUM][255] =
+	{
+		"data/title/all.png",
+		"data/title/start.png",
+
+		"data/title/mouse1.png",
+		"data/title/mouse2.png",
+	};
+
+	const int START_SIZE = 226;
+	const int MOUSE_SIZE = 111;
+
+private:
+	int handle[IMAGE_MAX_NUM];
+	int mouse;
+
+	int x[IMAGE_MAX_NUM], y[IMAGE_MAX_NUM];
 };
